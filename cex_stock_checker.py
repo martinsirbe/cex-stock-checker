5677700765a2c6bc6858f1d102754ec38a781644
Martins <martins.git@irbe.co.uk>
2016-05-31 21:20:37 +0100

    Adding logic to check a specific store stock. Also changed the logic to use item titles instead of item IDs. - Adding option in config yaml to choose to disable the prompt to proceed.

--- cex_stock_checker.py
+++ cex_stock_checker.py
@@ -2,7 +2,9 @@ import yaml
 import requests
 from time import sleep
 
-OUT_OF_STOCK = "Out Of Stock"
+PROMPT_DISABLED = "proceedPromptDisabled"
+
+ITEM_IN_STOCK_TAG_PATTERN = "\">{}</a></h1>"
 
 INPUT_YES = "y"
 INPUT_NO = "n"
@@ -10,11 +12,15 @@ INPUT_NO = "n"
 CONFIG_YAML = "config.yaml"
 CONFIG_REQUEST_DELAY = "requestDelay"
 CONFIG_ITEMS = "items"
+CONFIG_STORE_ID = "storeId"
+
+ANY_STORE_URL = "https://uk.webuy.com/search/index.php?stext={}&section=&is=1"
+SPECIFIC_STORE_URL = "https://uk.webuy.com/search/index.php?stext={}&section=&rad_which_stock=3&refinebystore={}&is=1"
 
 CHECK_URL = "https://uk.webuy.com/product.php?sku={}#."
 PROCEED_PROMPT_MSG = "Do you wish to continue to check the stock for {} items? (y/n)"
-OUT_OF_STOCK_MSG = "Item with ID {} is currently out of stock."
-IN_STOCK_MSG = "Item with ID {} is in stock."
+OUT_OF_STOCK_MSG = "-  {} is currently out of stock."
+IN_STOCK_MSG = "+  {} is in stock."
 ABORT_MSG = "Aborting the stock check."
 WRONG_INPUT_MSG = "Sorry, I didn't get that, please choose either y or n"
 ITEM_COUNT_MSG = "Currently there are {} items in your check list."
@@ -24,13 +30,15 @@ def check_stock(proceed):
     if proceed == INPUT_YES:
         for item in items_to_check:
             sleep(float(request_delay))
-            item_id = str(item)
-            response = requests.get(CHECK_URL.format(item_id))
+            item_title = str(item)
 
-            if OUT_OF_STOCK in response.text:
-                print(OUT_OF_STOCK_MSG.format(item_id))
+            response = get_request(item_title)
+
+            if ITEM_IN_STOCK_TAG_PATTERN.format(item_title) in response.text:
+                print(IN_STOCK_MSG.format(item_title))
             else:
-                print(IN_STOCK_MSG.format(item_id))
+                print(OUT_OF_STOCK_MSG.format(item_title))
+
     elif proceed == INPUT_NO:
         print(ABORT_MSG)
     else:
@@ -38,15 +46,30 @@ def check_stock(proceed):
         check_stock(input(PROCEED_PROMPT_MSG.format(item_count)))
 
 
+# Make a get request to Cex with given item title.
+def get_request(item_title):
+    if store_id is None:  # By default store_id is set to None, therefore will check all stores.
+        response = requests.get(ANY_STORE_URL.format(item_title))
+    else:  # Will check specific store stock for given item.
+        response = requests.get(SPECIFIC_STORE_URL.format(item_title, store_id))
+    return response
+
+
 with open(CONFIG_YAML, "r") as stream:
     try:
         config = yaml.load(stream)
         items_to_check = config[CONFIG_ITEMS]
         request_delay = config[CONFIG_REQUEST_DELAY]
+        store_id = config[CONFIG_STORE_ID]
+        promptDisabled = config[PROMPT_DISABLED]
 
         item_count = str(len(items_to_check))
         print(ITEM_COUNT_MSG.format(item_count))
 
-        check_stock(input(PROCEED_PROMPT_MSG.format(item_count)))
+        if promptDisabled:
+            check_stock(INPUT_YES)
+        else:
+            check_stock(input(PROCEED_PROMPT_MSG.format(item_count)))
+
     except yaml.YAMLError as exception:
         print(exception)
